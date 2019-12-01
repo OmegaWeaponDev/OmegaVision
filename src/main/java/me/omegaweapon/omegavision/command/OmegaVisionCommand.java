0529c0318a7a2530ac522e55455698bd8a8f95dd
Jamie Gordon <jamiegordon115@gmail.com>
2019-12-01 21:59:43 +1030

    OmegaVision - Add backwards Compatibility

--- src/main/java/me/omegaweapon/omegavision/command/OmegaVisionCommand.java
+++ src/main/java/me/omegaweapon/omegavision/command/OmegaVisionCommand.java
@@ -6,16 +6,15 @@ import me.omegaweapon.omegavision.settings.utils.ColourUtils;
 import me.omegaweapon.omegavision.settings.utils.ConfigSettings;
 import me.omegaweapon.omegavision.settings.utils.MessageUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionType;
 
 public class OmegaVisionCommand implements CommandExecutor {
+
 	OmegaVision plugin;
 
 	public OmegaVisionCommand(OmegaVision pl) {
@@ -23,52 +22,55 @@ public class OmegaVisionCommand implements CommandExecutor {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-		if(commandSender instanceof Player) {
+	public boolean onCommand(CommandSender commandSender, Command command, String string, String[] args) {
+		if (commandSender instanceof Player) {
 			Player player = (Player) commandSender;
-			if(command.getName().equalsIgnoreCase("omegavision") && (commandSender.hasPermission("omegavision.toggle") || player.isOp())) {
-				if(args.length != 1) {
+			String version = Bukkit.getVersion();
+
+			if (command.getName().equalsIgnoreCase("omegavision") && (player.hasPermission("omegavision.toggle") || player.isOp())) {
+				if (args.length != 1) {
 					player.sendMessage(MessageUtils.getPrefix() + ColourUtils.Colourize("&b/ausvision toggle - Toggle your nightvision on or off"));
 					player.sendMessage(MessageUtils.getPrefix() + ColourUtils.Colourize("&b/ausvision reload - Reload the plugin"));
 					return true;
 				} else if (args[0].equalsIgnoreCase("toggle")) {
-					if(!player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
-						player.addPotionEffect(
-							new PotionEffect(
-								PotionEffectType.NIGHT_VISION,
-								Integer.MAX_VALUE,
-								1,
-								ConfigSettings.getAmbient(),
-								ConfigSettings.getParticles(),
-								ConfigSettings.getNightVisionIcon())
-						);
-						player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
-						if(ConfigSettings.getNightVisionLogin().equals(true) && (player.hasPermission("omegavision.login") || player.isOp())) {
-							PlayerData.getPlayerData().createSection(player.getUniqueId().toString());
-							PlayerData.getPlayerData().set(player.getUniqueId().toString() + "." + "NightVision", true);
-							PlayerData.savePlayerData();
+					if (!player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
+						if (version.contains("1.7") || version.contains("1.8") || version.contains("1.9") || version.contains("1.10") || version.contains("1.11") || version.contains("1.12")) {
+							player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+							player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
+							if (ConfigSettings.getNightVisionLogin().equals(true) && (player.hasPermission("omegavision.login") || player.isOp())) {
+								PlayerData.getPlayerData().createSection(player.getUniqueId().toString());
+								PlayerData.getPlayerData().set(player.getUniqueId().toString() + "." + "NightVision", false);
+								PlayerData.savePlayerData();
+							}
+						} else {
+							player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1, ConfigSettings.getAmbient(), ConfigSettings.getParticles(), ConfigSettings.getNightVisionIcon()));
+							player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
+							if (ConfigSettings.getNightVisionLogin().equals(true) && (player.hasPermission("omegavision.login") || player.isOp())) {
+								PlayerData.getPlayerData().createSection(player.getUniqueId().toString());
+								PlayerData.getPlayerData().set(player.getUniqueId().toString() + "." + "NightVision", false);
+								PlayerData.savePlayerData();
+							}
 						}
 						return true;
 					} else {
 						player.removePotionEffect(PotionEffectType.NIGHT_VISION);
 						player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionRemoved());
-						if(ConfigSettings.getNightVisionLogin().equals(true) && (player.hasPermission("omegavision.login") || player.isOp())) {
+						if (ConfigSettings.getNightVisionLogin().equals(true) && (player.hasPermission("omegavision.login") || player.isOp())) {
 							PlayerData.getPlayerData().createSection(player.getUniqueId().toString());
 							PlayerData.getPlayerData().set(player.getUniqueId().toString() + "." + "NightVision", false);
 							PlayerData.savePlayerData();
 						}
+						return true;
 					}
-					return true;
 				} else if(args[0].equalsIgnoreCase("reload")) {
 					if(player.hasPermission("omegavision.reload") || player.isOp())
-					plugin.reloadConfig();
+						plugin.reloadConfig();
 					PlayerData.reloadPlayerData();
 					player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getReloadCommand());
 					return true;
+				} else {
+					player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNoPermission());
 				}
-			} else {
-				commandSender.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNoPermission());
-				return true;
 			}
 		}
 		return true;
--- src/main/java/me/omegaweapon/omegavision/events/PlayerListener.java
+++ src/main/java/me/omegaweapon/omegavision/events/PlayerListener.java
@@ -6,22 +6,19 @@ import me.omegaweapon.omegavision.settings.PlayerData;
 import me.omegaweapon.omegavision.settings.utils.ConfigSettings;
 import me.omegaweapon.omegavision.settings.utils.MessageUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.DyeColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+
 public class PlayerListener implements Listener {
 	OmegaVision plugin;
-	final PluginDescriptionFile pdf = OmegaVision.getInstance().getDescription();
+	String version = Bukkit.getVersion();
 
 	public PlayerListener(OmegaVision plugin) {
 		this.plugin = plugin;
@@ -29,22 +26,27 @@ public class PlayerListener implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
-		// Setup all the variables
 		Player player = playerJoinEvent.getPlayer();
+
 		Boolean nightVision = PlayerData.getPlayerData().getBoolean(player.getUniqueId() + ".NightVision");
 
 		// Applies or removes nightvision onjoin
 		if(nightVision.equals(true) && ConfigSettings.getNightVisionLogin().equals(true)) {
-			player.addPotionEffect(
-				new PotionEffect(
-					PotionEffectType.NIGHT_VISION,
-					Integer.MAX_VALUE,
-					1,
-					ConfigSettings.getAmbient(),
-					ConfigSettings.getParticles(),
-					ConfigSettings.getNightVisionIcon()
-				)
-			);
+			if(version.contains("1.7") || version.contains("1.8") || version.contains("1.9") || version.contains("1.10") || version.contains("1.11") || version.contains("1.12")) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+				player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
+			} else {
+				player.addPotionEffect(
+					new PotionEffect(
+						PotionEffectType.NIGHT_VISION,
+						Integer.MAX_VALUE,
+						1,
+						ConfigSettings.getAmbient(),
+						ConfigSettings.getParticles(),
+						ConfigSettings.getNightVisionIcon())
+				);
+				player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
+			}
 		} else if (nightVision.equals(false) ||  ConfigSettings.getNightVisionLogin().equals(false)) {
 			player.removePotionEffect(PotionEffectType.NIGHT_VISION);
 			if(player.hasPermission("omegavision.login")) {
@@ -67,18 +69,25 @@ public class PlayerListener implements Listener {
 		Player player = e.getPlayer();
 		Boolean nightVision = PlayerData.getPlayerData().getBoolean(player.getUniqueId() + ".NightVision");
 
-		player.sendMessage("NightVision = " + ConfigSettings.getKeepNightVision());
-
 		// Add nightvision to player after the respawn, depending on config and playerdata settings
 		if(ConfigSettings.getKeepNightVision().equals(true) && nightVision.equals(true) && player.hasPermission("omegavision.death")) {
-			Bukkit.getScheduler().runTaskLater(plugin, () -> player.addPotionEffect(new PotionEffect(
-				PotionEffectType.NIGHT_VISION,
-				Integer.MAX_VALUE,
-				1,
-				ConfigSettings.getAmbient(),
-				ConfigSettings.getParticles(),
-				ConfigSettings.getNightVisionIcon()
-			)), (1));
+			Bukkit.getScheduler().runTaskLater(plugin, () -> {
+					if(version.contains("1.7") || version.contains("1.8") || version.contains("1.9") || version.contains("1.10") || version.contains("1.11") || version.contains("1.12")) {
+						player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
+						player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
+					} else {
+						player.addPotionEffect(
+							new PotionEffect(
+								PotionEffectType.NIGHT_VISION,
+								Integer.MAX_VALUE,
+								1,
+								ConfigSettings.getAmbient(),
+								ConfigSettings.getParticles(),
+								ConfigSettings.getNightVisionIcon())
+						);
+						player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
+					}
+			}, (1));
 			player.sendMessage(MessageUtils.getPrefix() + MessageUtils.getNightvisionApplied());
 		}
 	}
